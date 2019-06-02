bb8644fe1d1b66d1a6aad875c9c1998114079b5e
yuriki <3dyuriki@gmail.com>
2019-06-02 10:56:10 +0300

    Fix iOS id

--- Assets/Scripts/UI/GoWriteReview.cs
+++ Assets/Scripts/UI/GoWriteReview.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class GoWriteReview : MonoBehaviour
 {
 	string androidUrl = "market://details?id=com.yuriki3d.PocketMoneyMath";
-	string ios7Url = "itms-apps://itunes.apple.com/app/idcom.3dyuriki.PocketMoneyMath?action=write-review&mt=8";
-	string iosUrl = "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=com.3dyuriki.PocketMoneyMath&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software";
+	string ios7UrlOld = "itms-apps://itunes.apple.com/app/idcom.3dyuriki.PocketMoneyMath?action=write-review&mt=8";
+	string iosUrlOld = "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=com.3dyuriki.PocketMoneyMath&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software";
+	string ios7Url = "itms-apps://itunes.apple.com/app/id1372434662?action=write-review&mt=8";
+	string iosUrl = "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=1372434662&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software";
 
 	public void RateApp()
 	{
